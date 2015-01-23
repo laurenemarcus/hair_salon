c698cb4ed6ba10e4bbd9a123f1dfd5c4a6c83af4
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-23 10:28:54 -0800

    Save stylists to the database.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,19 +1,28 @@
 class Stylist
-  attr_reader(:id, :stylist_name)
+  attr_reader(:stylist_name, :id)
 
   define_method(:initialize) do |attributes|
-    @id = attributes.fetch("id")
-    @stylist_name = attributes.fetch("stylist_name")
+    @stylist_name = attributes.fetch(:stylist_name)
+    @id = attributes.fetch(:id).to_i()
   end
 
   define_singleton_method(:all) do
     stylists = []
     returned_stylists = DB.exec("SELECT * FROM stylists;")
     returned_stylists.each() do |stylist|
-      id = stylist.fetch("id").to_i()
       stylist_name = stylist.fetch("stylist_name")
-      stylists.push(Stylist.new(:id => id, :stylist_name => stylist_name))
+      id = stylist.fetch("id").to_i()
+      stylists.push(Stylist.new({:stylist_name => stylist_name, :id => id}))
     end
     stylists
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (stylist_name) VALUES ('#{@stylist_name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_stylist|
+    self.stylist_name().==(another_stylist.stylist_name()).&(self.id().==(another_stylist.id()))
   end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -6,4 +6,20 @@ describe("Stylist") do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("saves stylists to the database") do
+      test_stylist = Stylist.new({:stylist_name => "Sherri", :id => nil})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same stylist if it has the same name") do
+      test_stylist = Stylist.new({:stylist_name => "Sherri", :id => nil})
+      test_stylist2 = Stylist.new({:stylist_name => "Sherri", :id => nil})
+      expect(test_stylist).to(eq(test_stylist2))
+    end
+  end
 end
