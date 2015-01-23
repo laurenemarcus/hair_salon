65afab05e4f57337b31c1b37b866f5bdeac9869d
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-23 12:02:14 -0800

    All a salon owner to input the name of a stylist and see a list of stylists.

--- app.rb
+++ app.rb
@@ -11,3 +11,11 @@ get("/") do
   @stylists = Stylist.all()
   erb(:index)
 end
+
+post("/stylists") do
+  stylist_name = params.fetch("stylist_name")
+  stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
