b7b95c3dc8c6e28074e14ce5c1ed66639fcb3f07
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-23 11:33:12 -0800

    Save clients into the database.

--- lib/client.rb
+++ lib/client.rb
@@ -1,7 +1,7 @@
 class Client
   attr_reader(:client_name, :stylist_id)
 
-  define_method(:intialize) do |attributes|
+  define_method(:initialize) do |attributes|
     @client_name = attributes.fetch(:client_name)
     @stylist_id = attributes.fetch(:stylist_id)
   end
@@ -15,5 +15,13 @@ class Client
       clients.push(Client.new({:client_name => client_name, :stylist_id => stylist_id}))
     end
     clients
+  end
+
+  define_method(:==) do |another_client|
+    self.client_name().==(another_client.client_name()).&(self.stylist_id().==(another_client.stylist_id()))
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO clients (client_name, stylist_id) VALUES ('#{client_name}', #{stylist_id})")
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,4 +6,27 @@ describe("Client") do
       expect(Client.all()).to(eq([]))
     end
   end
+
+  describe("#==") do
+    it("is the same client if it has the same client name and stylist id") do
+      test_client = Client.new({:client_name => "Judith", :stylist_id => 1})
+      test_client2 = Client.new({:client_name => "Judith", :stylist_id => 1})
+      expect(test_client).to(eq(test_client2))
+    end
+  end
+
+  describe("#stylist_id") do
+    it("lets you read the stylist ID out") do
+      test_client = Client.new({:client_name => "Judith", :stylist_id => 1})
+      expect(test_client.stylist_id()).to(eq(1))
+    end
+  end
+
+  describe("#save") do
+    it("adds a client to the array of saved clients") do
+      test_client = Client.new({:client_name => "Judith", :stylist_id => 1})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
 end
